a63e22f7cf2acdc52c97004d245c19c1693a5217
VetoPlayer <andrewlloys@gmail.com>
2020-11-29 11:50:41 +0100

    python: enable python autocompletion

--- install_vim.sh
+++ install_vim.sh
@@ -6,9 +6,19 @@ set -e
 mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
-# Install pylint and python3-autopep8
-sudo apt-get install python3-autopep8 pylint
+# Install vim-nox for python3 extension support, pylint and python3-autopep8
+sudo apt-get install vim-nox python3-autopep8 pylint
 
+# Install jedi for autocompletion support
+sudo apt-get install vim-python-jedi 
+
+## You need to have the jedi library installed in you venv (or systemwide)
+pip install jedi
+
+# Look for information
+apt-cache show vim-python-jedi
+
+# vim-addons install python-jedi` will create all needed symlinks in ~/.vim/
 # TODO: Checkout vim-pylint package 
 
 echo "Linting: Remember pylint has to be installed inside the virtual environment!"
